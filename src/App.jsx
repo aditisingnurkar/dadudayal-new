5f07a527d929209dba97e465b3070827db4e8c97
aditisingnurkar <aditi.singnurkar@gmail.com>
2026-03-30 09:47:49 +0530

    projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginScreen from './LoginScreen'
 import Dashboard from './Dashboard'
+import Projects from './Projects'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginScreen />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/"          element={<LoginScreen />} />
+        <Route path="/dashboard" element={<Dashboard />}   />
+        <Route path="/projects"  element={<Projects />}    />
       </Routes>
     </BrowserRouter>
   )
