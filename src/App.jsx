a359be519473af9b636bfc635c4b4f1b931c3aff
aditisingnurkar <aditi.singnurkar@gmail.com>
2026-03-30 09:23:56 +0530

    added dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-import LoginScreen from "./LoginScreen";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginScreen from './LoginScreen'
+import Dashboard from './Dashboard'
 
 export default function App() {
-  return <LoginScreen />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginScreen />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
